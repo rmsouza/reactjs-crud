52e68dbba0d3e7f151e2cc63f9540747e2fd61ae
Rafael Menezes <rafaelmenezes86@gmail.com>
2019-05-29 23:28:24 -0400

    Config react table

--- src/components/EmployeeList/index.js
+++ src/components/EmployeeList/index.js
@@ -1,32 +1,71 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import AddIcon from '@material-ui/icons/Add'
+import { Link } from 'react-router-dom'
+import history from '../../history'
+import { deleteEmployee } from '../../actions'
+
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
 
 class EmployeeList extends Component {
+  handleEdit = (employee) => {
+    history.push(`/employees/edit?id=${employee.id}`)
+  }
+
+  handleDelete = (employee) => {
+    this.props.deleteEmployee(employee.id)
+  }
+
   render () {
     const { employees } = this.props
 
     if (employees.length) {
       return (
-        <div>
-          <h1>Employees List</h1>
+        <React.Fragment>
+          <Typography variant="h5" component="h3">
+            Employees
 
-          {
-            employees.map(employee => {
-              return (
-                <div key={ employee.id }>
-                  <p>{ employee.name }</p>
-                </div>
-              )
-            })
-          }
-        </div>
-      )
+            <Button variant="contained" color="primary" component={Link} to="/employees/create">
+              <AddIcon />
+              New Employee
+            </Button>
+          </Typography>
+
+          <ReactTable
+            data={employees}
+            columns={[
+              { Header: "Name", accessor: "name" },
+              { Header: "Code", accessor: "code" },
+              { Header: "Profession", accessor: "profession" },
+              { Header: "Color", accessor: "color" },
+              { Header: "City", accessor: "city" },
+              { Header: "Branch", accessor: "branch" },
+              {
+                Header: '',
+                Cell: row => (
+                  <div>
+                    <button onClick={() => this.handleEdit(row.original)}>Edit</button>
+                    <button onClick={() => this.handleDelete(row.original)}>Delete</button>
+                  </div>
+                )
+              }
+            ]}
+            defaultPageSize={10}
+            className="-striped -highlight"
+          />
+        </React.Fragment>
+      )  
     } else {
-      return (<h1>No employees</h1>)
+      return (<React.Fragment />)
     }
+    
   }
 }
 
 const mapStateToProps = (state) => ({ employees: state.employees })
+const mapDispatchToProps = { deleteEmployee }
 
-export default connect(mapStateToProps)(EmployeeList)
+export default connect(mapStateToProps, mapDispatchToProps)(EmployeeList)
